eca7c951979e394f34c0ee33e2929359253f10af
honeypatel11 <honeypatel1109@gmail.com>
2024-10-14 16:09:02 +0530

    it is Assortment exam

---
+++ exam-5/Q-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     int arr , size , max ;
+
+    cout << "enter the size of array : ";
+    cin >> size;
+
+    for(int  i=0; i < size; i++ ){
+        cout << "enter the value : " ;
+        cin >> arr;
+       
+    }
+    for(int  i=0; i <size; i++ ){
+        cout << arr << " ";  
+    }
+   for (int  i = 0; i < size; i++)
+   {
+    if(size != 0 && size < 0);
+    cout << "negative num is";
+   }
+   
+
+    return 0;
+}
---
+++ exam-5/Q-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int arr[5] , size , neg=0 ;
+
+    cout << "enter the size of array : ";
+    cin >> size;
+
+    for(int  i=0; i <5; i++ ){
+        cout << "enter the value : " ;
+        cin >> arr[i];
+       
+    }
+    for(int  i=0; i <5; i++ ){
+        cout << arr[i] << " ";  
+    }
+    
+   for (int i = 0; i < neg; i++)
+   
+ 
+   
+       
+    
+   
+
+
+
+ 
+
+    return 0;
+}
